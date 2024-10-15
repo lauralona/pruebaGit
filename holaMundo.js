6a3735c0fe5507ef54fb6349a84ab261993d67e6
lauralona <laura.lopez@iesjulianmarias.es>
2024-10-15 19:32:04 +0200

    Update holaMundo.js

--- holaMundo.js
+++ holaMundo.js
@@ -1,2 +1,2 @@
 console.log("Hola mundo");
-document.getElementById("texto").innerHTML+="y Javascript prueba a editar desde github"
+document.getElementById("texto").innerHTML+="y Javascript prueba a editar desde github prueba 2 a ver si se marca"
